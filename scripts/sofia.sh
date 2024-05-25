172076cbb86074858f11e167df502aede82104b4
Felipe Pimenta <felipe.pbernardo@gmail.com>
2024-05-25 17:57:47 -0300

    adds deafult value to second param

--- scripts/sofia.sh
+++ scripts/sofia.sh
@@ -3,14 +3,15 @@
 # Path to the Sofia directory
 SOFIA_DIR="$HOME/.sofia"
 
+# Default output path
+OUTPUT_DEFAULT="./output.tsv"
+
 function sofia() {
   # Check if the argument is provided
   if [ $# -eq 0 ]; then
     echo "Expected 1 argument for the file path"
   else
     # Pass the argument to the npm script
-    echo "1 - $1"
-    echo "2 - $2"
-    node $SOFIA_DIR/scripts/run.js "$1" "$2"
+    node $SOFIA_DIR/scripts/run.js "$1" "${2:-$OUTPUT_DEFAULT}"
   fi
 }
