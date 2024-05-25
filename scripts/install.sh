de85f89802cd82c40e84d868e08a008d0528009a
Felipe Pimenta <felipe.pbernardo@gmail.com>
2024-05-25 12:52:21 -0300

    updates install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
+# Define the directory path
+SOFIA_DIR="~/.sofia"
 
-git clone https://github.com/felipepimentab/sofia.git ~/.sofia
-cd ~/.sofia
+# Check if the directory exists
+if [ -d "" ]; then
+  echo "Directory $SOFIA_DIR exists. Deleting it and its contents..."
+  
+  # Remove the directory and its contents
+  rm -rf "$SOFIA_DIR"
+  
+  # Verify the deletion
+  if [ ! -d "$SOFIA_DIR" ]; then
+    echo "Directory $SOFIA_DIR was successfully deleted."
+  else
+    echo "Failed to delete the directory $SOFIA_DIR."
+  fi
+else
+  echo "Directory $SOFIA_DIR does not exist."
+fi
+
+# Clone git repository into ~/.sofia
+git clone https://github.com/felipepimentab/sofia.git $SOFIA_DIR
+cd $SOFIA_DIR
+
+# Checks for and installs required dependencies
 chmod +x ./scripts/deps.sh
 sh ./scripts/deps.sh
-npm run script
+
+echo "Sofia has been successfuly installed!"
