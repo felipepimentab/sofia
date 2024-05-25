374cb4b05b843ad263994750942ef8a3869014ce
Felipe Pimenta <felipe.pbernardo@gmail.com>
2024-05-25 17:41:16 -0300

    finishes run script

--- scripts/run.js
+++ scripts/run.js
@@ -1,25 +1,159 @@
 import {  writeFile, readFile } from 'fs';
 
-(async () => {
+/**
+ * Gets the content of the line following the matched line
+ * @param {string[]} arr 
+ * @param {string} str 
+ * @returns {string}
+ */
+function getNextLine(arr, str) {
+  const i = arr.findIndex(el => el.includes(str));
+  if(!!arr[i+2] && !headers.some(s => arr[i+2].includes(s))) {
+    return arr[i+1] + ' ' + arr[i+2];
+  } else {
+    return i>0 ? arr[i+1] : '';
+  }
+}
+
+/**
+ * Gets the content of the same line after a ": "
+ * @param {string[]} arr 
+ * @param {string} str 
+ * @returns {string}
+ */
+function getSameLine(arr, str) {
+  const line = arr.find(el => el.includes(str));
+  return line ? line.split(': ')[1] : '';
+}
+
+const headers = [
+  'Título atribuído',
+  'Outras remetências de título:',
+  'Categorias',
+  'Material original',
+  'Ano',
+  'País',
+  'Cidade',
+  'Estado',
+  'Sinopse',
+  'Termos descritores',
+  'Descritores secundários',
+  'Termos geográficos',
+  'Produção',
+  'Conteúdo examinado',
+  'Fontes utilizadas:',
+  'Fontes consultadas:',
+  'Observações:',
+]
+
+class Film {
+  constructor(titulo, tituloAlt, categorias, material, ano, pais, cidade, estado, sinopse, termosDescritores, descritoresSecundarios, termosGeograficos, producao, conteudoExaminado, fontesExaminadas, fontesConsultadas, obervacoes) {    
+    this.titulo = titulo;
+    this.tituloAlt = tituloAlt;
+    this.categorias = categorias;
+    this.material = material;
+    this.ano = ano;
+    this.pais = pais;
+    this.cidade = cidade;
+    this.estado = estado;
+    this.sinopse = sinopse;
+    this.termosDescritores = termosDescritores;
+    this.descritoresSecundarios = descritoresSecundarios;
+    this.termosGeograficos = termosGeograficos;
+    this.producao = producao;
+    this.conteudoExaminado = conteudoExaminado;
+    this.fontesExaminadas = fontesExaminadas;
+    this.fontesConsultadas = fontesConsultadas;
+    this.obervacoes = obervacoes;
+  }
+}
+
+function createFilmFromText(text) {
+  /**
+   * @description Array with a string for each line
+   * @type Array<string>
+   */
+  const lines = text.split(/\r?\n/);
+
+  const titulo = lines[0];
+  const tituloAlt = getNextLine(lines, 'Outras remetências de título:');
+  const categorias = getNextLine(lines, 'Categorias');
+  const material = getNextLine(lines, 'Material original');
+  const ano = getSameLine(lines, 'Ano');
+  const pais = getSameLine(lines, 'País');
+  const cidade = getSameLine(lines, 'Cidade');
+  const estado = getSameLine(lines, 'Estado');
+  const sinopse = getNextLine(lines, 'Sinopse');
+  const termosDescritores = getNextLine(lines, 'Termos descritores');
+  const descritoresSecundarios = getNextLine(lines, 'Descritores secundários');
+  const termosGeograficos = getNextLine(lines, 'Termos geográficos');
+  const producao = getNextLine(lines, 'Produção');
+  const conteudoExaminado = getSameLine(lines, 'Conteúdo examinado');
+  const fontesExaminadas = getNextLine(lines, 'Fontes utilizadas:');
+  const fontesConsultadas = getNextLine(lines, 'Fontes consultadas:');
+  const obervacoes = getNextLine(lines, 'Observações:');
+
+  return new Film(
+    titulo,
+    tituloAlt,
+    categorias,
+    material,
+    ano,
+    pais,
+    cidade,
+    estado,
+    sinopse,
+    termosDescritores,
+    descritoresSecundarios,
+    termosGeograficos,
+    producao,
+    conteudoExaminado,
+    fontesExaminadas,
+    fontesConsultadas,
+    obervacoes
+  );
+}
+
+
+function convertToTSV(data) {
+  const header = headers.join('\t')
+  const rows = data.map(obj => Object.values(obj).join("\t"));
+  return `${header}\n${rows.join("\n")}`;
+}
+
+(async function () {
   const INPUT_FILE_PATH = process.argv[2];
+  const OUTPUT_FILE_PATH = process.argv[3] || './example/output.tsv';
 
   const text = await new Promise((resolve, reject) => {
     readFile(INPUT_FILE_PATH, (err, data) => {
       if (err || !data) reject(new Error(`\x1b[31mFailed\x1b[0m to read file at ${INPUT_FILE_PATH}.`));
       else {
-        console.log(`\x1b[32mSuccess\x1b[0m reading file at ${INPUT_FILE_PATH}`);
+        console.log(`\x1b[32mSuccessfuly\x1b[0m read file at ${INPUT_FILE_PATH}`);
         const str = data.toString('utf-8');
         resolve(str);
       }
     });
-  })
+  });
 
   const splitRegex = /\n\d+\/629\n/;
-  const header = text.split(splitRegex).slice(0,1);
-  const items = text.split(splitRegex).slice(1);
+  const body = text.split(splitRegex).slice(1);
+  let films = [];
 
-  items.forEach(item => {
-    console.log(item);
-    console.log('------------------')
+  body.forEach(film => {
+    films.push(createFilmFromText(film));
+  });
+
+  const tsv = convertToTSV(films);
+
+  await new Promise((resolve, reject) => {
+    writeFile(OUTPUT_FILE_PATH, tsv, function (err) {
+      if (err) {
+        reject(new Error(`\x1b[31mFailed\x1b[0m to create .tsv file.`));
+      } else {
+        console.log(`\x1b[32mSuccessfuly\x1b[0m created .tsv file.`);
+        resolve();
+      }
+    });
   });
 })();
--- scripts/sofia.sh
+++ scripts/sofia.sh
@@ -1,45 +1,13 @@
 #!/bin/bash
 
-
-# Define a function to display usage information
-show_help() {
-  echo "Usage: $0 [options]"
-  echo "Options:"
-  echo "  --help    Display this help message"
-  # Add more options here if needed
-}
-
 function sofia() {
-  # Parse command-line options
-  while getopts ":h-:" opt; do
-    case $opt in
-      h)
-        show_help
-        ;;
-      -)
-        case "${OPTARG}" in
-          help)
-            show_help
-            ;;
-          *)
-            echo "Invalid option: --${OPTARG}"
-            show_help
-            ;;
-        esac
-        ;;
-      \?)
-        echo "Invalid option: -$OPTARG"
-        show_help
-        ;;
-    esac
-  done
-
   # Check if the argument is provided
   if [ $# -eq 0 ]; then
     echo "Expected 1 argument for the file path"
   else
-    cd ..
     # Pass the argument to the npm script
-    node run.js -- "$1"
+    echo "1 - $1"
+    echo "2 - $2"
+    npm run sofia -- "$1" "$2"
   fi
 }
